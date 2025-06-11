da7d2b7777361e06c5d5d640e555c99fb295d91d
littleman222 <asmaayubtayyab@gmail.com>
2025-06-11 14:52:28 +0200

    A2.py

---
+++ A2.py
@@ -0,0 +1,12 @@
+# Creating diffrent variables 
+x =5
+y = "John"
+print(x)
+print(y)
+
+codingal = "condingal"
+print(codingal)
+# User Input
+name = input("enter your name: ")
+
+print("/nHello", name, "/nwelcome to codingal")
