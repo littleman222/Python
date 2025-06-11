95274218a5953e1fc44178f49c2d81f0716baae4
littleman222 <asmaayubtayyab@gmail.com>
2025-06-11 14:35:48 +0200

    A1.py

---
+++ A1.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(4)
+
+#Next line after print
+print("Hello Phyton /n")
+
+#Pass multiple argumentsfor printing
+print("Hello",5)
+
+#End argument of Print Statement
+print("welcome to", end= "*")
