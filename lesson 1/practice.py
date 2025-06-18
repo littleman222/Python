96959f1336d91b63f4c78ad63f3d9505284bab03
littleman222 <asmaayubtayyab@gmail.com>
2025-06-18 14:46:10 +0200

    A1.py

---
+++ lesson 1/practice.py
@@ -0,0 +1,5 @@
+print(" birthdays of five friends\n")
+
+# Storing birthdays in variablesand printing them
+a="12/03/95"
+print("aakasj -",a)
---
+++ lesson 2/A1.py
@@ -0,0 +1,26 @@
+# Assigning diffrent variables
+name = "Penguin"
+age= 15
+is_student = True
+weight = 38.5
+
+# Printing diffrent variables Variables and their data type
+print("name :", name)
+print("data type of name is", type(name))
+
+print("Age :", age)
+print("data type of age is", type(age))
+
+print("is_student :", is_student)
+print("data type of is_student is", type(is_student))
+
+print("Weight :", weight)
+print("data type of weight is", type(weight))
+
+# type casting to conbert the datatype of variables print("\n after type casting...........") 
+age = str(age)
+print(age)
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is", type(weight))
