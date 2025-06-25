2386e2fa6e05cffe85bbe5de9fe1ec9bedb918f0
littleman222 <asmaayubtayyab@gmail.com>
2025-06-25 16:00:39 +0200

    A1.py

---
+++ lesson 2/A2.py
@@ -0,0 +1,9 @@
+#input a word
+text = str(input("enter a string"))
+
+# reverse string
+# using step value as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
+
+print()
---
+++ lesson 3/A1.py
@@ -0,0 +1,14 @@
+# Storing values
+tree1 = 98
+tree2 = 23
+tree3 = 94
+tree4 = 20
+tree5 = 37
+
+# Finding out total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all the 5 trees is: ", sum)
+
+# Finding the average of trees
+average = sum/5
+print("the average of all the tree is:", average)
