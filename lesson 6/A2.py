c77e6e6fc9a16c90b8df2d69226c187171fb3af6
littleman222 <asmaayubtayyab@gmail.com>
2025-07-30 16:23:12 +0200

    A2.py

---
+++ lesson 6/A2.py
@@ -0,0 +1,9 @@
+#Input a word or sentence
+string =input("Please enter your own String: ")
+string = ("")
+#loop for printing in reverse
+for i in string:
+    string2 = i + string2
+
+print("\nThe original String = ", string)
+print("The Reversed String= ", string2)
